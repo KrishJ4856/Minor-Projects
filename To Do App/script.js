6437e0d3af3c06d64faef3f5ad10c03d75918fde
Krish4856 <98867214+Krish4856@users.noreply.github.com>
2022-12-01 21:18:27 +0530

    Styling added

--- To Do App/script.js
+++ To Do App/script.js
@@ -1,6 +1,44 @@
-if(Storage.length !== 0){
+const body = document.querySelector("body");
+const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+const light = document.querySelector("#light");
+const dark = document.querySelector("#dark");
 
+if(darkTheme.matches){
+    dark.style.display = "none";
+    light.style.display = "block";
+    const message = document.createElement("div");
+    message.innerHTML = "System Theme: Dark";
+    message.className = "message";
+    body.appendChild(message);
+    setTimeout(()=> {
+        body.removeChild(message);
+    }, 1000)
 }
+else{
+    dark.style.display = "block";
+    light.style.display = "none";
+    const message = document.createElement("div");
+    message.innerHTML = "System Theme: Light";
+    message.className = "message";
+    body.appendChild(message);
+    setTimeout(()=> {
+        body.removeChild(message);
+    }, 1000)
+}
+
+light.addEventListener("click", () => {
+    light.style.display = "none";
+    dark.style.display = "block";
+    body.style.color = "black";
+    body.style.backgroundColor = "white";
+})
+
+dark.addEventListener("click", () => {
+    dark.style.display = "none";
+    light.style.display = "block";
+    body.style.color = "white";
+    body.style.backgroundColor = "black";
+})
 
 const inputBox = document.querySelector("#inputBox");
 const tasks = document.querySelector("#tasks");
@@ -29,6 +67,7 @@ function addTask(e){
 
         const taskInfo = document.createElement("h3");
         taskInfo.innerHTML = inputBox.value;
+        taskInfo.style.overflowX = "auto";
         inputBox.value = "";
         
         newTask.appendChild(taskInfo);
